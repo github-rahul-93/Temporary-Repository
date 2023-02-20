1a6322cf8bc18f1b00c379cb3d707645c62a5813
github-rahul-93 <errahuldas1993@gmail.com>
2023-02-20 21:46:59 +0530

    Updated Latest

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const a = "Rahul is a bull"
+const a = 'How are you?'
 
 
-console.log(a(3))
+console.log(a.charAt(2))
 console.log(a.length)
+console.log(a.slice(4,7))
+
+
+const b = 'How are you?'
+
+
+console.log(b.charAt(2))
+console.log(b.length)
