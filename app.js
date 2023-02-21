e214d9187f5b03d6e6337e5940134b8aaae3fa41
github-rahul-93 <errahuldas1993@gmail.com>
2023-02-21 23:02:11 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const a = '     My name is Pubali     '
+const a = 'My name is Pubali'
 
 
 console.log(a.charAt(6))
+
 console.log(a.length)
+
 console.log(a.slice(16,23))
+
 console.log(a.split())
 
 let b = 'Priya'
 let c = 'loves'
 let d = 'Rahul'
-console.log(b,c,d)
+let e = '34'
+
+console.log(b,c,d,e)
