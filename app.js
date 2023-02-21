9831fd842a48f929e7f4623ceb2edf1166031b74
github-rahul-93 <errahuldas1993@gmail.com>
2023-02-21 22:58:45 +0530

    Love Commit

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const a = '     My name is Pubali     '
 console.log(a.charAt(6))
 console.log(a.length)
 console.log(a.slice(16,23))
-console.log(a.split(a))
+console.log(a.split())
 
-const b = Priya
-const c = loves
-const d = Rahul
-console.log(b+c+d)
+let b = 'Priya'
+let c = 'loves'
+let d = 'Rahul'
+console.log(b,c,d)
