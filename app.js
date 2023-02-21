6b7b4ba92ebaad7e2602f04bcfd352fe18b9d3ec
github-rahul-93 <errahuldas1993@gmail.com>
2023-02-21 22:47:58 +0530

    Four Output

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const a = 'How are you?'
+const a = '     My name is Pubali     '
 
 
-console.log(a.charAt(2))
+console.log(a.charAt(6))
 console.log(a.length)
-console.log(a.slice(4,7))
-
-
-const b = 'How are you?'
-
-
-console.log(b.charAt(2))
-console.log(b.length)
+console.log(a.slice(16,23))
+console.log(a.split(a))
